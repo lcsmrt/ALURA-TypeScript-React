5da9c468d0a2e6aed37b4502bcd9bb4b4d83e5bb
Lucas Martins <lcsmrtakd@gmail.com>
2022-10-10 13:50:44 -0300

    simple form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Button from './components/Button';
+import Form from './components/Form';
 function App() {
   return (
     <div className="App">
-      <Button/>
+      <Form />
     </div>
   );
 }
