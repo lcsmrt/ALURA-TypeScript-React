566be07f8f7b7e33b2d9075c99369e6e975b5752
lcsmrt <lcsmrtakd@gmail.com>
2022-10-10 17:02:26 -0300

    finished the basic structure

---
+++ src/components/List/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function List() {
+  const tasks = [{
+    task: 'React',
+    time: '02:00:00'
+  }, {
+    task: 'TypeScript',
+    time: '01:00:00'
+  }]
+  return (
+    <aside>
+      <h2> Estudos do dia </h2>
+      <ul>
+        {tasks.map((item, index) => (
+          <li key={index}>
+            <h3> {item.task} </h3>
+            <span> {item.time} </span>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  )
+}
+
+export default List;
