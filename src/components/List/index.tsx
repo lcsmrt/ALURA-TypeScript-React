f52c6daa8d6c7d12b71258cd45a2cef6769aa31b
Lucas Martins <lcsmrtakd@gmail.com>
2022-10-11 11:22:15 -0300

    changed list to use state

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,17 +1,23 @@
+import { useState } from 'react';
 import Item from './Item/item';
 import style from './List.module.scss';
 
 export default function List() {
-  const tasks = [{
+  const [tasks, setTasks] = useState([{
     task: 'React',
     time: '02:00:00'
   }, {
-    task: 'TypeScript',
+    task: 'JavaScript',
     time: '01:00:00'
-  }]
+  }, {
+    task: 'TypeScript',
+    time: '00:45:00'
+  }])
   return (
     <aside className={style.taskList}>
-      <h2> Estudos do dia </h2>
+      <h2 onClick={() => {
+        setTasks([...tasks, { task: "Conceito de state", time: "00:45:00"}]);
+      }}> Estudos do dia </h2>
       <ul>
         {tasks.map((item, index) => (
           <Item 
